412df8054d52cee34b85ed1c1862e1ffd7f28963
GabrielaVieira7 <131814122+gabrielaVieira7@users.noreply.github.com>
2023-05-23 10:42:15 -0300

    Add files via upload

---
+++ GabrielaVieira_Exercicio01.java
@@ -0,0 +1,22 @@
+/*Todos os números entre 0 e 1000
+Autora: Gabriela
+Data: 23/05/2023
+ */
+
+ public class GabrielaVieira_Exercicio01 {
+    public static void main (String args []){
+
+        //Criando a variável
+        int num;
+
+        //Iniaclizando a variável
+        num = 0;
+
+
+        while (num <= 1000) {
+            System.out.println(num);
+
+            num++;
+        }
+    }
+ }
