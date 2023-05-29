891ee10476b28d96ab3e02eb3e953d295ee4add3
GabrielaVieira7 <131814122+gabrielaVieira7@users.noreply.github.com>
2023-05-29 17:56:52 -0300

    GabrielaVieira_Exercicio04.java

--- GabrielaVieira_Exercicio04.java
+++ GabrielaVieira_Exercicio04.java
@@ -4,12 +4,12 @@ Data: 29/5/2023
 */
 public class GabrielaVieira_Exercicio04 {
     public static void main(String[] args) {
-        
-
+       
+        //Mostrando o que irá acontecer no algoritmo
         System.out.println("Números ímpares entre 0 e 1000:");
 
-        for (int i = 1; i <= 1000; i += 2) {
-            System.out.println(i);
+        for (int val = 1; val <= 1000; val += 2) {
+            System.out.println(val);
         }
     }
 }
