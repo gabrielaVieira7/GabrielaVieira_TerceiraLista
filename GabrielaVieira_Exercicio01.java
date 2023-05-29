faf66786f20745d76e4cf87d26160a7d33e8bd99
GabrielaVieira7 <131814122+gabrielaVieira7@users.noreply.github.com>
2023-05-29 17:15:38 -0300

    Update GabrielaVieira_Exercicio01.java

--- GabrielaVieira_Exercicio01.java
+++ GabrielaVieira_Exercicio01.java
@@ -5,16 +5,16 @@ Data: 23/05/2023
 
  public class GabrielaVieira_Exercicio01 {
     public static void main (String [] args){
-
-        //Criando a variável
-        int num;
-
-        //Iniaclizando a variável
-        num = 0;
-
-
+        
+          //Criando a variável e inicializando-a
+        int num = 0;
+    
+        //Mostrando o que ira acontecer no algoritmo
+        System.out.println("Todos os números entre 0 e 1000");
+    
         while (num <= 1000) {
             System.out.println(num);
+            
 
             num++;
         }
