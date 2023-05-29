a88d2b4b4fc05886fe219a9dafe42009595fdc54
GabrielaVieira7 <131814122+gabrielaVieira7@users.noreply.github.com>
2023-05-29 14:12:08 -0300

    Add files via upload

---
+++ GabrielaVieira_Exercicio02.java
@@ -0,0 +1,45 @@
+/*leia dois valores e mostre todos os números entre eles
+Autora: Gabriela
+Data: 22/05/2023
+*/
+
+import java.util.Scanner;
+
+public class GabrielaVieira_Exercicio02 {
+    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        
+         //inicia as variáveis
+         int val1 = 0;
+         int val2 = 0;
+         int tes = 0;
+
+         //Lê as variáveis
+
+        System.out.println("Digite um número inteiro: ");
+        val1 = leitor.nextInt();
+        
+        System.out.println("Digite outro número inteiro: ");
+        val2 = leitor.nextInt();
+        
+        System.out.println("Números entre " + val1 + " e " + val2 + ":");
+        
+       
+        if (val1 <= val2) {
+            tes = val1 + 1;
+            do {
+                System.out.println(tes);
+                tes++;
+            } while (tes < val2);
+        } else {
+            tes = val1 - 1;
+            do {
+                System.out.println(tes);
+                tes--;
+            } while (tes > val2);
+
+            leitor.close();
+        }
+    }
+}
